0b48f17e978d26c386953828711390c98c6d9195
John Albritton <jtareb1@att.net>
2015-01-16 16:26:20 -0500

    initial commit

--- main.js
+++ main.js
@@ -2,14 +2,44 @@ var items, pricetag, currencytype
 
 /*first qeustion*/
 
-var total_prictag = 0;
-var avg_prictag = 0;
-items.filter(function(a) {
-	total_pricetag +=a.price;
+var total_pricetag = 0;
+var avg_pricetag = 0;
+items.filter(function(a){
+	total_pricetag +=	a.price;
 
 });
-avg_pricetag =total_pricetag/itms.length;
-console.log(avg_pricetag);
+avg_pricetag =total_pricetag/items.length;
+console.log('The average price is', avg_pricetag);
+
+
+/*second question*/
+var items, title, price, currency_code;
+
+var pricetag=items.filter(function(object) {
+	
+ 	 return object.price>= 14 && object.price <= 18;  
+ 		
+ 				
+ 	
+});
+
+console.log(pricetag);
+
+
+
+
+var titlegbp = [];
+var title, price, currency_code
+	var currencytype=items.filter(function(object) {
+		if(object.currency_code === "GBP"){
+		titlegbp.push(object.title + object.price);
+		};
+	 
+		
+
+	});
+
+console.log('The name of the product using the GBP is', titlegbp);
 
 
 
