2b63e3915a07ef812399f7b6b82a0f955c630d5d
John Albritton <jtareb1@att.net>
2015-01-16 08:12:46 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-var items, price
+var items, pricetag, currencytype
+
+/*first qeustion*/
+
+var total_prictag = 0;
+var avg_prictag = 0;
+items.filter(function(a) {
+	total_pricetag +=a.price;
+
+});
+avg_pricetag =total_pricetag/itms.length;
+console.log(avg_pricetag);
+
+
 
 
 
